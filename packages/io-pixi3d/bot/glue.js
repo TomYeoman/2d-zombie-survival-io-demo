15e2fcc0f4e5ed5c37c7522f547c71598813d3b0
whatisaghostwriter <sunracarrera@gmail.com>
2022-05-11 05:15:17 +0000

    removed pyramids

--- packages/io-pixi3d/bot/glue.js
+++
@@ -1,4 +0,0 @@
-require = require('esm')(module/*, options*/)
-module.exports.nengi = require('nengi')
-module.exports.nengiConfig = require('../common/nengiConfig')
-module.exports.MoveCommand = require('../common/command/MoveCommand')
--- packages/io-pixi3d/client/hooks/createHooks.js
+++
@@ -1,9 +0,0 @@
-import obstacleHooks from './obstacleHooks.js'
-import playerHooks from './playerHooks.js'
-
-export default (state, renderer) => {
-    return {
-        'PlayerCharacter': playerHooks(state, renderer),
-        'Obstacle': obstacleHooks(state, renderer)
-    }
-}
